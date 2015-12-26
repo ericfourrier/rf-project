00aeb6d58ad20abcd204b39bc249136da7619093
Eric Fourrier <ericfourrier@gmail.com>
2015-12-26 01:12:54 +0100

    clean code

--- get_intraday_price.py
+++ get_intraday_price.py
@@ -13,6 +13,7 @@ import pandas as pd
 import datetime as dt
 import numpy as np
 
+
 class GoogleFinanceStock(object):
     root_url = "http://www.google.com/finance/getprices"
 
@@ -28,32 +29,32 @@ class GoogleFinanceStock(object):
         payload = {'q': self.ticker, 'p': '{}d'.format(self.window), 'f': self.col,
                    'i': self.period, 'df': self.df}
         # split into lines
-        res = requests.get(
-            self.root_url, params=payload).text.strip().split('\n')
+        res = requests.get(self.root_url, params=payload).text.strip().split('\n')
         self._meta = {l.split('=')[0]: l.split('=')[1] for l in res[1:7]}
         self._data = pd.DataFrame([l.split(',') for l in res[7:]], columns=self._meta[
                                   'COLUMNS'].split(','))
         self._meta['START_TIME_RAW'] = int(self._data.loc[0, 'DATE'][1:])
         self._meta['START_TIME'] = dt.datetime.fromtimestamp(self._meta['START_TIME_RAW'])
-        index_day = self._data.loc[:,'DATE'].str.startswith('a')
+        index_day = self._data.loc[:, 'DATE'].str.startswith('a')
         # create auxiliary columns for day change
         self._data['DATE_D'] = np.nan
-        self._data.loc[index_day,'DATE_D'] = self._data.loc[index_day, 'DATE'].str.replace('a','').map(lambda x :int(x))
+        self._data.loc[index_day, 'DATE_D'] = self._data.loc[
+            index_day, 'DATE'].str.replace('a', '').map(lambda x: int(x))
         self._data = self._data.fillna(method='ffill')
         # replace start_time for by 0 for better
         self._data.loc[index_day, 'DATE'] = 0
-        self._data = self._data.astype(float) # convert everything to float
+        self._data = self._data.astype(float)  # convert everything to float
         if convert_to_datetime:
-            self._data['DATE'] = (int(self._meta['INTERVAL']) * self._data['DATE']) + self._data['DATE_D'] - int(360*60)
-            self._data['DATE'] = self._data['DATE'].map(lambda x: dt.datetime.fromtimestamp(x))
+            self._data['DATE'] = (int(self._meta['INTERVAL']) * self._data['DATE']) + self._data['DATE_D'] - int(360 * 60)
+            self._data['DATE'] = self._data['DATE'].map(
+                lambda x: dt.datetime.fromtimestamp(x))
         return self._data, self._meta
 
 
-
 if __name__ == "__main__":
-    g1 = GoogleFinanceStock('GOOG') # 1 days history
+    g1 = GoogleFinanceStock('GOOG')  # 1 days history
     res = g1.get_data()
     print(res[0])
-    g1.window = 10 # 10 days history
+    g1.window = 10  # 10 days history
     res2 = g1.get_data()
     print(res2[0])
